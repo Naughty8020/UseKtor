76bde669a4ff22a0304cfe30cf25477e2e8f3549
naughty8020 <169634290+Naughty8020@users.noreply.github.com>
2026-04-29 20:46:06 +0900

    add:plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(ktorLibs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 group = "com.example"
@@ -17,6 +18,11 @@ dependencies {
     implementation(ktorLibs.server.config.yaml)
     implementation(ktorLibs.server.core)
     implementation(ktorLibs.server.netty)
+
+    // libs.versions.toml で定義したものは libs. で参照
+    implementation(libs.server.content.negotiation)      // ← 追加
+    implementation(libs.serialization.kotlinx.json)      // ← libs. に変更
+
     implementation(libs.logback.classic)
 
     testImplementation(kotlin("test"))
