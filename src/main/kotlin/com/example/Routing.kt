1cdcaf7177533ac6495bedee4f9463d64f45ff69
naughty8020 <169634290+Naughty8020@users.noreply.github.com>
2026-05-01 15:39:51 +0900

    add:JWT-auth

--- src/main/kotlin/com/example/Routing.kt
+++ src/main/kotlin/com/example/Routing.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import com.auth0.jwt.JWT
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -7,48 +8,70 @@ import com.example.models.Books
 import io.ktor.server.request.receive
 import com.example.models.Book
 import io.ktor.http.HttpStatusCode
+import com.example.models.User
+import io.ktor.server.auth.*
+import java.util.Date
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello, World!")
         }
-        get("/allBooks") {
-            val books = Books.getAllBooks()
-            call.respond(books)
+
+        post("/login") {
+            val user = call.receive<User>()
+            val secret = environment.config.property("jwt.secret").getString()
+            val issuer = environment.config.property("jwt.issuer").getString()
+            val audience = environment.config.property("jwt.audience").getString()
+
+            val token = JWT.create()
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .withClaim("username", user.username)
+                .withExpiresAt(Date(System.currentTimeMillis() + 600000))
+                .sign(com.auth0.jwt.algorithms.Algorithm.HMAC256(secret))
+
+            call.respond(mapOf("token" to token))
         }
 
-        post("/addBooks"){
-            val inputJson = call.receive<Book>()
-            Books.addBook(inputJson)
-            call.respond(HttpStatusCode.Created, "Book added successfully")
-        }
+        authenticate("auth-jwt") {
+            get("/allBooks") {
+                val books = Books.getAllBooks()
+                call.respond(books)
+            }
 
-        put("/{id}"){
-            val id = call.parameters["id"]?.toInt()
-            if(id == null){
-                call.respond(HttpStatusCode.BadRequest, "Book name is required")
-                return@put
+            post("/addBooks") {
+                val inputJson = call.receive<Book>()
+                Books.addBook(inputJson)
+                call.respond(HttpStatusCode.Created, "Book added successfully")
             }
-            val inputJson = call.receive<Book>()
-            if(Books.updateBook(id, inputJson) > 0){
-                call.respond(HttpStatusCode.OK, "Book updated successfully")
-            } else {
-                call.respond(HttpStatusCode.NotFound, "Book not found")
-            }
-        }
 
-        delete("/{id}"){
-            val id = call.parameters["id"]?.toInt()
-            if(id == null){
-                call.respond(HttpStatusCode.BadRequest, "Book name is required")
-                return@delete
+            put("/{id}") {
+                val id = call.parameters["id"]?.toInt()
+                if (id == null) {
+                    call.respond(HttpStatusCode.BadRequest, "ID is required")
+                    return@put
+                }
+                val inputJson = call.receive<Book>()
+                if (Books.updateBook(id, inputJson) > 0) {
+                    call.respond(HttpStatusCode.OK, "Book updated successfully")
+                } else {
+                    call.respond(HttpStatusCode.NotFound, "Book not found")
+                }
             }
-            if(Books.deleteBook(id) > 0){
-                call.respond(HttpStatusCode.OK, "Book deleted successfully")
-            } else {
-                call.respond(HttpStatusCode.NotFound, "Book not found")
+
+            delete("/{id}") {
+                val id = call.parameters["id"]?.toInt()
+                if (id == null) {
+                    call.respond(HttpStatusCode.BadRequest, "ID is required")
+                    return@delete
+                }
+                if (Books.deleteBook(id) > 0) {
+                    call.respond(HttpStatusCode.OK, "Book deleted successfully")
+                } else {
+                    call.respond(HttpStatusCode.NotFound, "Book not found")
+                }
             }
         }
-   }
+    }
 }
